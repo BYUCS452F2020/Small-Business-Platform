de9481c26d21f898bc0879a033a71b335edb9459
Nate Teahan <nateteahan@gmail.com>
2020-10-14 17:09:41 -0600

    Updated login component

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import '../styles/Login.css'
 // import img from '../assets/image.jpg'
 
-const LoginComponent : React.FC = () => {
+const LoginComponent: React.FC = () => {
   const history = useHistory()
 
   const handleLogin = () => {
@@ -15,7 +15,7 @@ const LoginComponent : React.FC = () => {
       <h1>SPACKLE</h1>
       {/* <img src={img} alt="logo" /> */}
       <div onClick={handleLogin}>
-        <button className="button-style"> Login </button>
+        <button className="button-style"> Get Started </button>
       </div>
 
     </div>
