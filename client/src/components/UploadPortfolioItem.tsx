b3a297d99fd5a08b089b8238cf85c38456f448d4
Nate Hood <nate@Nates-MacBook-Pro.local>
2020-10-24 15:45:58 -0600

    upload portfolio item UI completed

--- client/src/components/UploadPortfolioItem.tsx
+++ client/src/components/UploadPortfolioItem.tsx
@@ -7,12 +7,12 @@ interface Props {backend: Backend}
 
 const UploadPortfolioItem: React.FC<Props> = ({backend}: Props) => {
   const [description, setDescription] = useState<string>('')
-  const [portfolioFile, setPortfolioFile] = useState<File|null>(null)
+  const [previewFile, setPreviewFile] = useState<string|null>('')
 
   async function handleSubmit() {
     const portfolioItem = {
       description: description,
-      file: portfolioFile
+      file: previewFile
     }
   
     try{
@@ -23,31 +23,52 @@ const UploadPortfolioItem: React.FC<Props> = ({backend}: Props) => {
     }
   }
 
+  async function setFile(file: File | null) {
+    let fileBase64: Promise<string|null>;
+    if (file) {
+      fileBase64 = new Promise(resolve => {
+        const reader = new FileReader()
+        reader.addEventListener('load', () => {
+          resolve(reader.result && reader.result.toString())
+        })
+        reader.readAsDataURL(file)
+      })
+    } else {
+      fileBase64 = Promise.resolve(null)
+    }
+    let filePreview = await fileBase64
+    setPreviewFile(filePreview || null)
+  }
+
   return(
     <section className='portfolio-upload'>
-      <textarea 
-        className='description' 
-        placeholder="Tell me about this"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}/>
-      <br/>
-      <hr className='horrizontal-line'/>
-      <FileUpload
-          label="Portfolio item"
-          accept={['.jpg', '.png']}
-          onChange={(file) =>{
-            if (file !== null){
-              setPortfolioFile(file)
-            }
-          }} 
-        />
+      <h2>Upload Portfolio Item</h2>
+      <div className='text-image-block'>
+        <textarea 
+          className='description' 
+          placeholder="Tell me about this"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}/>
+        <div className='image-block'>
+          {previewFile && 
+              <img src={previewFile} className='image-preview' alt="Portfolio image"/>
+          }
+        <FileUpload
+            htmlAttrs={{
+              className: 'portolio-file-upload'
+            }}
+            label="Portfolio image"
+            accept={['.jpg', '.png']}
+            onChange={(file) => setFile(file)}/>
+        </div>
+      </div>
       <button
-          className="btn-plain smaller"
-          type="submit"
-          disabled={!description || !portfolioFile}
-          onClick={handleSubmit}>
-          Publish
-        </button>
+        className="btn-plain smaller"
+        type="submit"
+        disabled={!description || !previewFile}
+        onClick={handleSubmit}>
+        Publish
+      </button>  
     </section>
   )
 }
--- client/src/types/portfolioItem.ts
+++ client/src/types/portfolioItem.ts
@@ -1,4 +1,4 @@
 export default interface PortfolioItem {
   description: string,
-  file: File | null
+  file: string | null
 }
